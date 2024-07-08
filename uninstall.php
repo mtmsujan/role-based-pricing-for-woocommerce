af71fd3ff30290c9d97a8ad6d33a4e31d8de5d54
mtmsujan <mtmsujon@gmail.com>
2024-07-08 17:00:34 +0600

    initial commit

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Summary (no period for file headers)
+ *
+ * Description. (use period)
+ *
+ * @link       https://wordpress.org/plugins/woocommerce-role-based-price/
+ * @package    Role Based Price For WooCommerce
+ * @subpackage Role Based Price For WooCommerce/core
+ * @since      3.0
+ */
+if( ! defined('WP_UNINSTALL_PLUGIN') ) {
+    exit;
+}
